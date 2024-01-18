749097edb2c5b860637df2482f67b012a57f9059
nhdthnh <nhudinhthanh1010@gmail.com>
2024-01-18 23:00:12 +0700

    'abc'

--- Personal_GUI.py
+++ Personal_GUI.py
@@ -1,93 +1,87 @@
-from tkinter import *
 import tkinter as tk
-from tkinter import font,ttk,Label
-import os
-from Support_Function import *
-import datetime
-import sys
 import pandas as pd
-import threading
-from openpyxl import load_workbook
-# Path Config
-USS_CENTRALIZATION_PATH         = r'\\bosch.com\dfsRB\DfsVN\LOC\Hc\RBVH\20_EDA\10_EDA9\01_Internal\10_Tool\00_ToolChain_Template'
-    # Path Logo
-LOG_PATH                        = os.path.join(USS_CENTRALIZATION_PATH, 'Log')
-LOGO_PATH                       = os.path.join(USS_CENTRALIZATION_PATH, 'Icon')
+import os
 
-# DBC_GENERATION_PATH             = os.path.join(USS_CENTRALIZATION_PATH,'1.DBC_Generation')
+from tkinter import *
+from tkinter import colorchooser
 
-# IMPORT_DBC_PATH                 = os.path.join(USS_CENTRALIZATION_PATH,'2.Import_DBC')
-# IMPORT_DBC_FOLDER_PATH          = os.path.join(IMPORT_DBC_PATH, 'Import DBC')
-# IMPORT_DBC_LINK_PATH            = os.path.join(IMPORT_DBC_PATH,'README.txt')
+BUTTON_CONFIG_XLSX_PATH = os.path.join(os.getcwd(), "Button_Config.xlsx")
 
-# VSM_GENERATION_PATH             = os.path.join(USS_CENTRALIZATION_PATH,'3.VSM_Generation')
-# VSM_GENERATION_LINK_PATH        = os.path.join(VSM_GENERATION_PATH,'README.txt')
-
-# TX_OFFSETTUNNING_PATH           = os.path.join(USS_CENTRALIZATION_PATH,'4.Tx_OffsetTunning')
-# TX_OFFSETTUNNING_LINK_PATH      = os.path.join(TX_OFFSETTUNNING_PATH,'README.txt')
-
-# LAB_MONITORING_PATH             = os.path.join(USS_CENTRALIZATION_PATH,'5.Lab_Remote')
-# VSM_CONVERSION_ASW_PATH         = os.path.join(USS_CENTRALIZATION_PATH,'6.VSM_ConversionASW')
-# OPL_TEMPLATE_PATH               = os.path.join(USS_CENTRALIZATION_PATH,'7.OPL_Template')
-# REVIEW_CHECKLIST_PATH           = os.path.join(USS_CENTRALIZATION_PATH,'8.Review_Checklist')
-# FH_DESIGN_TEMPLATE_PATH         = os.path.join(USS_CENTRALIZATION_PATH,'9.FH_Template')
-# RESOURCE_MEASUREMENT_PATH       = os.path.join(USS_CENTRALIZATION_PATH,'10.ResourceManagement_Template')
-# DIAG_COMMON_UNDERSTANDING_PATH  = os.path.join(USS_CENTRALIZATION_PATH,'11.DiagCommonUnderstanding')
-# FIM_DESIGN_TEMPLATE_PATH        = os.path.join(USS_CENTRALIZATION_PATH,'12.FiM_Design_Template')
-
-# RUNTIME_CHECKLIST_PATH          = os.path.join(USS_CENTRALIZATION_PATH,'13.Runtime_Checklist')
-# RUNTIME_CHECKLIST_LINK_PATH     = os.path.join(RUNTIME_CHECKLIST_PATH,'README.txt')
-
-# DOORS_UPDATE_PATH               = os.path.join(USS_CENTRALIZATION_PATH,'14.Doors_update_requirement_linking')
-
-# FAQ_PATH                        = os.path.join(USS_CENTRALIZATION_PATH,'FAQ')
-# FAQ_LINK_PATH                   = os.path.join(FAQ_PATH,'README.txt')
-
-TOOLS_VERSION_PATH              = os.path.join(USS_CENTRALIZATION_PATH,'Tools_Version')
-USS_KIT_VERSION_PATH            = os.path.join(TOOLS_VERSION_PATH,'USS_KIT_Version.txt')
-
-USS_KIT                         = os.path.join(USS_CENTRALIZATION_PATH,'USS_KIT')
-
-CONFIG_PATH                     = os.path.join(USS_CENTRALIZATION_PATH,'Config')
-BUTTON_CONFIG_XLSX_PATH         = os.path.join(os.getcwd(),'Button_Config.xlsx')
-STATE_CONFIG_PATH               = os.path.join(os.getcwd(),'State.txt')
-SW_VER = 'SW_1.0'
 def PersonalGUI():
-    def button_click(button_text):
-        # Xử lý sự kiện khi một nút được nhấn
-        print(f"Nút {button_text} đã được nhấn")
+    def add_button_popup():
+        def colorpicker_click():
+            c_code = colorchooser.askcolor()[1]  # Lấy mã màu hex từ tuple trả về
+            color_var.set(c_code)
 
-    # Đọc dữ liệu từ tệp Excel
-    file_path = BUTTON_CONFIG_XLSX_PATH  # Thay 'ten_file.xlsx' bằng đường dẫn thực tế của bạn
+        popup = tk.Toplevel(window)
+        popup.title("Thêm Nút")
+
+        def save_button():
+            new_button_name = entry1.get()
+            new_button_path = entry2.get()
+            button_color = color_var.get()
+
+            if new_button_name and new_button_path and button_color:
+                df.loc[len(df)] = [new_button_name, new_button_path, button_color]
+                df.to_excel(BUTTON_CONFIG_XLSX_PATH, sheet_name='Open_Folder', index=False)
+                update_gui()
+                popup.destroy()
+            else:
+                tk.messagebox.showerror("ERROR", "Please enter information.")
+
+        label1 = tk.Label(popup, text="button_name:")
+        label1.grid(row=0, column=0, padx=5, pady=5)
+        entry1 = tk.Entry(popup)
+        entry1.grid(row=0, column=1, padx=5, pady=5)
+
+        label2 = tk.Label(popup, text="Path:")
+        label2.grid(row=1, column=0, padx=5, pady=5)
+        entry2 = tk.Entry(popup)
+        entry2.grid(row=1, column=1, padx=5, pady=5)
+
+        color_label = tk.Label(popup, text="Màu:")
+        color_label.grid(row=2, column=0, padx=5, pady=5)
+        color_var = tk.StringVar()
+        color_var.set("#000000")  # Màu mặc định là đen
+        color_entry = tk.Entry(popup, textvariable=color_var, state='readonly')
+        color_entry.grid(row=2, column=1, padx=5, pady=5)
+        color_button = tk.Button(popup, text="Color", command=colorpicker_click)
+        color_button.grid(row=2, column=2, padx=5, pady=5)
+
+        save_button_button = tk.Button(popup, text="SAVE", command=save_button)
+        save_button_button.grid(row=3, column=0, columnspan=3, pady=10)
+
+    def button_click(button_text):
+        file_path = df.loc[df['button_name'] == button_text, 'path'].values[0]
+        os.startfile(file_path)
+       
+
+    file_path = BUTTON_CONFIG_XLSX_PATH
     try:
-        df = pd.read_excel(file_path, sheet_name='Open_Folder',usecols=['button_name'])
+        df = pd.read_excel(file_path, sheet_name='Open_Folder', usecols=['button_name', 'path', 'color'])
     except pd.errors.EmptyDataError:
-        # Trường hợp tệp Excel rỗng, tạo DataFrame trống
         df = pd.DataFrame()
 
-    # Tạo cửa sổ
     window = tk.Tk()
-    window.title("GUI với Nút 4x3 (Tự động thêm nút từ Excel)")
+    window.title("Personal GUI")
 
-    # Tạo lưới nút từ dữ liệu trong DataFrame
     buttons = df.values.tolist() if not df.empty else []
 
     def update_gui():
         for i in range(len(buttons)):
-            # Tính toán vị trí dòng và cột dựa trên số cột tối đa (max_columns)
-            row = i // max_columns
+            row = (i // max_columns)
             col = i % max_columns
-
-        
             button_value = buttons[i][0] if isinstance(buttons[i], list) else buttons[i]
-
-            button_text = str(button_value)  # Chuyển đổi giá trị thành chuỗi
-            btn = tk.Button(window, text=button_text, padx=20, pady=20, command=lambda text=button_text: button_click(text))
+            button_text = str(button_value)
+            button_color = buttons[i][2] if len(buttons[i]) > 2 else "#000000"  # Màu mặc định là đen
+            btn = tk.Button(window, text=button_text, width=15, height=5, command=lambda text=button_text: button_click(text), bg=button_color)
             btn.grid(row=row, column=col)
+
+        add_button = tk.Button(window, text="ADD BUTTON", command=add_button_popup)
+        add_button.grid(row=(len(buttons) // max_columns) + 1, column=len(buttons) % max_columns, padx=10, pady=10)
+
     max_columns = 4
-
-
-    # Cập nhật giao diện ban đầu
     update_gui()
     window.mainloop()
+
 PersonalGUI()
--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-import os
-os.startfile(r"C:\Users\NHH4HC\Desktop\Lab Monitoring.exe")
+from tkinter import *
+from tkinter import colorchooser
+
+def click():
+    color = colorchooser.askcolor()
+    colorHex = color[1]
+    window.config(bg=colorHex)
+
+window = Tk()
+window.geometry("420x420")
+button = Button(text='click me',command=click)
+button.pack()
+window.mainloop()
